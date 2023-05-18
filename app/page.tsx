b32879e914a7057801579542a04f3b6078f13e00
Joshua Marks <jmarks.joshua@googlemail.com>
2023-05-18 11:29:16 +0100

    Added error causing code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
+
+  const info = {
+    name: 'John',
+  }
+
+  const action = async () => {
+    'use server'
+    console.log(info.name)
+    const {name} = info
+    console.log(name)
+  }
+
   return null
 }
